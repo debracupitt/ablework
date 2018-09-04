f3a7bc08655cee25cc45fb9c3981542ea2ce3715
debc <debracupitt@gmail.com>
2018-09-04 13:41:48 +0700

    css tweaks

--- resources/js/ablework.js
+++ resources/js/ablework.js
@@ -3,18 +3,11 @@ $(document).ready(function() {
 const filterButton = document.querySelector('.filter');
 const filterForm = document.querySelector('.search-form');
 
-// function toggleOpen () {
-//   filterForm.classList.toggle('show');
-// }
-
-function viewForm () {
-  if (filterForm.style.display === "none") {
-    filterForm.style.display = "block";
-  } else {
-    filterForm.style.display = "none";
-  }
+function toggleOpen () {
+  // console.log(filterForm)
+  filterForm.classList.toggle('show');
 }
 
-filterButton.addEventListener('click', viewForm)
+filterButton.addEventListener('click', toggleOpen)
 
 });
